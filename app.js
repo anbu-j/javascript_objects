3b833bb02037d1362d13ecb3351fcb273fd4118a
PSADMIN <psadmin@psadmins-air-8.attlocal.net>
2021-05-16 20:27:07 -0400

    first commit

---
+++ app.js
@@ -0,0 +1,16 @@
+//objects calling method
+var self = {
+name: "Anbu Joselin",
+age: 31,
+DOB: "08/22/1989",
+BIRTHPLACE:"India",
+favColors:["red","white","orange","violet"]
+dreamCar:{
+    make:"Toyota",
+    model:"Sienna",
+    year:"2020",
+}
+}
+   //console.log(self)
+    //console.log(self.favColors)
+console.log(self.dreamCar.model)
